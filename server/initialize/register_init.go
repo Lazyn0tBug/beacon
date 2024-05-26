335c2c931736cba843c8d995104174a13ebeeebf
Lazyn0tBug <fuzombo@hotmail.com>
2024-05-27 00:56:37 +0800

    add initialize code

---
+++ server/initialize/register_init.go
@@ -0,0 +1,10 @@
+package initialize
+
+import (
+	_ "github.com/Lazyn0tBug/beacon/server/source/example"
+	_ "github.com/Lazyn0tBug/beacon/server/source/system"
+)
+
+func init() {
+	// do nothing,only import source package so that inits can be registered
+}
